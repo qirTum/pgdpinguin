7d7cdf4e96d88a863f640880dcf0a25f032432d0
qirTum <54902908+qirTum@users.noreply.github.com>
2019-09-04 17:34:00 +0200

    Create deploy.sh
    Created deploy.sh copy from affeio website

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+md2html()
+{
+	pandoc --mathjax --template=$srcdir/template.html \
+		$srcdir/$1.md -o $wwwdir/$1.html
+}
+
+srcdir=/home/affe/affe.io/
+wwwdir=/var/www/html/
+remote=halle:/u/halle/gieset/home_page/html-data/
+
+echo -n 'Exporting to local server... '
+rm $wwwdir/*
+
+for srcfile in $srcdir/*.md; do
+	md2html $(basename $srcfile .md)
+done
+
+cp $srcdir/*.css $wwwdir
+chmod 644 $wwwdir/*
+echo 'Done.'
+
+echo -n 'Transferring to remote server... '
+scp $wwwdir/* $remote
+echo 'Done.'
